d15db6072932b6da5b6cc86c253db653e33b4089
Giorgioeab <giorgioeabarbosa@gmail.com>
2025-03-16 20:54:48 -0300

    refactor: converts components to class

--- src/App.js
+++ src/App.js
@@ -1,31 +1,78 @@
-import { useState } from "react";
+import React from "react";
 import TaskItem from "./components/TaskItem";
 
-const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      description: "Learn React",
-      isCompleted: true,
-    },
-    {
-      id: 2,
-      description: "Learn Vue",
-      isCompleted: true,
-    },
-    {
-      id: 3,
-      description: "Learn Angular",
-      isCompleted: false,
-    },
-  ]);
-  return (
-    <>
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task}/>
-      ))}
-    </>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.state = {
+      tasks: [
+        {
+          id: 1,
+          description: "Learn React",
+          isCompleted: true,
+        },
+        {
+          id: 2,
+          description: "Learn Vue",
+          isCompleted: true,
+        },
+        {
+          id: 3,
+          description: "Learn Angular",
+          isCompleted: false,
+        },
+      ],
+    };
+  }
+  handleStateChange() {
+    this.setState({
+      tasks: [],
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.tasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
+        <button onClick={this.handleStateChange}>Limpar Tarefas</button>
+      </>
+    );
+  }
+}
 
 export default App;
+
+// import { useState } from "react";
+// import TaskItem from "./components/TaskItem";
+
+// const App = () => {
+//   const [tasks, setTasks] = useState([
+//     {
+//       id: 1,
+//       description: "Learn React",
+//       isCompleted: true,
+//     },
+//     {
+//       id: 2,
+//       description: "Learn Vue",
+//       isCompleted: true,
+//     },
+//     {
+//       id: 3,
+//       description: "Learn Angular",
+//       isCompleted: false,
+//     },
+//   ]);
+//   return (
+//     <>
+//       {tasks.map((task) => (
+//         <TaskItem key={task.id} task={task}/>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,10 +1,26 @@
-const TaskItem = ({ task }) => {
-  return (
-    <>
-      <h1>{task.description}</h1>
-      <p>{task.isCompleted ? "Você aprendeu" : "Você ainda não aprendeu"}</p>
-    </>
-  );
-};
+// const TaskItem = ({ task }) => {
+//   return (
+//     <>
+//       <h1>{task.description}</h1>
+//       <p>{task.isCompleted ? "Você aprendeu" : "Você ainda não aprendeu"}</p>
+//     </>
+//   );
+// };
+
+// export default TaskItem;
+
+import React from "react";
+
+class TaskItem extends React.Component {
+  render() {
+    const { task } = this.props;
+    return (
+      <>
+        <h1>{task.description}</h1>
+        <p>{task.isCompleted ? "Concluído" : "Não concluído"}</p>
+      </>
+    );
+  }
+}
 
 export default TaskItem;
