0ed2c85d3ca3a4c321533d21921734a0a44433c4
Giorgioeab <giorgioeabarbosa@gmail.com>
2025-03-19 22:28:53 -0300

    feat: add task item component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+
+import TaskItem from "./TaskItem";
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://almeida-task-manager-11f6877e11ed.herokuapp.com/tasks"
+      );
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+  return (
+    <div className="tasks-container">
+      <h2>Minhas Tarefas</h2>
+
+      <div className="last-tasks">
+        <h3>Últimas Tarefas</h3>
+        <div className="tasks-list">
+          {tasks
+            .filter((task) => task.isCompleted === false)
+            .map((lastTask) => (
+              <p>{lastTask.description}</p>
+            ))}
+        </div>
+      </div>
+
+      <div className="completed-tasks">
+        <h3>Tarefas Concluidas</h3>
+        <div className="tasks-list">
+        {tasks
+            .filter((task) => task.isCompleted)
+            .map((completedTask) => (
+              <p>{completedTask.description}</p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
