5f04f51d0a5b317f6160bed6c074d64953b8e991
Giorgioeab <giorgioeabarbosa@gmail.com>
2025-03-25 23:43:38 -0300

    feat: add function to add a task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,18 +1,46 @@
+import axios from "axios";
+import { useAlert } from "react-alert";
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 
 import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 
 const AddTask = () => {
   const [task, setTask] = useState("");
 
+  const alert = useAlert();
+
   const onChange = (e) => {
     setTask(e.target.value);
   };
+
+  const handleTaskAddition = async () => {
+    try {
+      if (task.length === 0) {
+        return alert.error(
+          "A tarefa precisa de uma descrição para ser addicionada."
+        );
+      }
+
+      await axios.post(
+        "https://almeida-task-manager-11f6877e11ed.herokuapp.com/tasks",
+        {
+          description: task,
+          isCompleted: false,
+        }
+      );
+    } catch (error) {}
+  };
+
   return (
     <div className="add-task-container">
       <CustomInput label="Adicionar tarefa" value={task} onChange={onChange} />
+      <CustomButton onClick={handleTaskAddition}>
+        <FaPlus size={14} color="#ffffff" />
+      </CustomButton>
     </div>
   );
 };
